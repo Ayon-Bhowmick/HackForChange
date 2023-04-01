f557efb1899d24c791b9105c78e3e04c713e3d8f
Hua Chen <huachen@dyn236114.cc.lehigh.edu>
2023-04-01 15:05:03 -0400

    Add more comments and route

--- backend/main.py
+++ backend/main.py
@@ -6,12 +6,25 @@ import database
 app = FastAPI()
 db = database.getDatatbase()
 
-@app.get("/")
+@app.get("/ping")
 def read_root():
-    return {"Hello": "World"}
+    return {"pong"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/pins")
+def getAllPins():
+    print(database.getAllPinData(db))
+    return database.getAllPinData(db)
+
+
+# Routes
+# 
+# 
+# 
+# 
+# 
+# 
