50f9b1a6be3082b5d8a2d0e9f5c4ae864e6be251
Ayon Bhowmick <ayb224@lehigh.edu>
2023-04-01 16:00:14 -0400

    bash

--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
 if [ "$1" = "--reqs"  || "$1" = "-r" ]; then
+    cd backend
     pip install pipreqs
 	pipreqs . --force
+    cd ..
 elif [ "$1" = "--install" || "$1" = "-i"]; then
+    cd backend
     pip install -r requirements.txt
+    cd ..
+    cd mobile
+    npm install
+    cd ..
 elif [ "$1" = "--start" || "$1" = "-s"]; then
     cd backend
     uvicorn main:api --reload
+    cd ..
+    cd mobile/forager
+    npm start
     cd ..
 fi
