df14c6e7c7093435ca662706182b1009472752ba
Ayon Bhowmick <ayb224@lehigh.edu>
2023-04-01 15:56:02 -0400

    bash

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
-if [ "$1" = "reqs" ]; then
+if [ "$1" = "--reqs"  || "$1" = "-r" ]; then
     pip install pipreqs
 	pipreqs . --force
-elif [ "$1" = "install" ]; then
-    
+elif [ "$1" = "--install" || "$1" = "-i"]; then
+    pip install -r requirements.txt
+elif [ "$1" = "--start" || "$1" = "-s"]; then
+    cd backend
+    uvicorn main:api --reload
+    cd ..
+fi
