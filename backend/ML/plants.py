09958d2353193afee8bea1f74deb984cd84d8762
Ayon Bhowmick <ayb224@lehigh.edu>
2023-04-01 20:57:45 -0400

    plants

---
+++ backend/ML/plants.py
@@ -0,0 +1,88 @@
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import logging
+import sys
+import pickle
+
+BUILD_DATA = True
+LOAD_MODEL = False
+
+class PlantDataMaker():
+    log = logging.getLogger("info")
+    TRAIN_PATH = "./plants/dataset"
+    TEST_PATH = "./plants/dataset-test"
+    training_data = []
+    testing_data = []
+    class_map = {}
+
+    def makeTrainingData(self):
+        self.log.info("Making training data")
+        for i, dir in enumerate(tqdm(os.listdir(self.TRAIN_PATH))):
+            self.class_map[i] = dir
+            for file in os.listdir(os.path.join(self.TRAIN_PATH, dir)):
+                try:
+                    path = os.path.join(self.TRAIN_PATH, dir, file)
+                    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                    # 1920x1080
+                    img = cv2.resize(img, (600, 600))
+                    img = cv2.copyMakeBorder(img, 2, 2, 2, 2, cv2.BORDER_CONSTANT, value=0)
+                    self.training_data.append([np.array(img), np.eye(62)[i]])
+                except Exception as e:
+                    self.log.error(e)
+        np.random.shuffle(self.training_data)
+        np.save("plant_training_data.npy", self.training_data)
+        with open("plant_class_map.pkl", "wb") as f:
+            pickle.dump(self.class_map, f)
+
+    def makeTestingData(self):
+        self.log.info("Making testing data")
+        for i, dir in enumerate(tqdm(os.listdir(self.TEST_PATH))):
+            for file in os.listdir(os.path.join(self.TRAIN_PATH, dir)):
+                try:
+                    path = os.path.join(self.TRAIN_PATH, dir, file)
+                    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                    # 1920x1080
+                    img = cv2.resize(img, (600, 600))
+                    img = cv2.copyMakeBorder(img, 2, 2, 2, 2, cv2.BORDER_CONSTANT, value=0)
+                    self.training_data.append([np.array(img), np.eye(62)[i]])
+                except Exception as e:
+                    self.log.error(e)
+        np.random.shuffle(self.training_data)
+        np.save("plant_testing_data.npy", self.training_data)
+
+class AyonNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+        self.conv4 = nn.Conv2d(128, 256, 5)
+        self.conv5 = nn.Conv2d(256, 512, 5)
+        self.fc1 = nn.Linear(512 * 5 * 5, 1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc3 = nn.Linear(1024, 1024)
+        self.fc4 = nn.Linear(1024, 512)
+        self.fc5 = nn.Linear(512, 62)
+
+    def convs(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv4(x)), (3, 3))
+        x = F.max_pool2d(F.relu(self.conv5(x)), (3, 3))
+        x = x.view(-1, 512 * 5 * 5)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout)
+    logging.getLogger("info").setLevel(logging.INFO)
+    plant = PlantDataMaker()
+    if BUILD_DATA:
+        plant.makeTrainingData()
+        plant.makeTestingData()
